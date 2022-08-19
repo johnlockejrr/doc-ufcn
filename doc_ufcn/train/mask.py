68af74a064a804b2f478139ec47372be98c06cd2
NolanB <nboukachab@teklia.com>
2022-08-19 18:41:15 +0200

    Draft tests for generate mask : diff image < 1

--- doc_ufcn/train/mask.py
+++ doc_ufcn/train/mask.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from itertools import combinations
-# from shapely.affinity import scale
-from shapely.geometry import LineString, MultiPolygon, Polygon
+
 import cv2
 import numpy
+from shapely.affinity import scale
+from shapely.geometry import LineString, MultiPolygon, Polygon
+
 
 def generate_mask(image_width, image_height, label_polygons, label_colors, output_path):
 
-    image_path = str(output_path)[:-4] + "_mask.jpg"
+    image_path = str(output_path)[:-4] + "_mask.png"
     print(image_path)
     black_img = numpy.zeros(
         (image_height, image_width, 3),
@@ -24,38 +26,37 @@ def generate_mask(image_width, image_height, label_polygons, label_colors, outpu
     picture_polygons = [Polygon(poly) for poly in picture_polygons]
     # Resize the polygons
     # line_polygons = resize_polygons(polygons=line_polygons, height=1, width=1)
-    # picture_polygons = resize_polygons(polygons=picture_polygons, height=1, width=1)
+    # picture_polygons = resize_polygons(polygons=picture_polygons, height=1.008, width=1.008)
     # Split the polygons
     line_polygons = split_polygons(line_polygons)
     picture_polygons = split_polygons(picture_polygons)
-
-    img = cv2.imwrite(image_path, black_img)
+    cv2.imwrite(image_path, black_img)
     img = cv2.imread(image_path)
-    for line_poly in line_polygons:
-      line_contours = [numpy.array(line_poly.exterior.coords).round().astype(numpy.int32)]
-      for line_contour in line_contours:
-          if len(line_contour) > 0:
-            img = cv2.drawContours(img, line_contours, 0, tuple(reversed(line_color)), -1)
-      cv2.imwrite(image_path, img)
+    draw_polygons(line_polygons, img, image_path, line_color)
+    draw_polygons(picture_polygons, img, image_path, picture_color)
 
-    for pict_poly in picture_polygons:
-      pict_contours = [numpy.array(pict_poly.exterior.coords).round().astype(numpy.int32)]
-      for contour in pict_contours:
-          if len(contour) > 0:
-            img = cv2.drawContours(img, pict_contours, 0, tuple(reversed(picture_color)), -1)
-      cv2.imwrite(image_path, img)
 
-# def resize_polygons(polygons: list, height: float, width: float) -> list:
-#     """
-#     Resize the polygons.
-#     :param polygons: The polygons to resize.
-#     :param height: The ratio in the height dimension.
-#     :param width: The ratio in the width dimension.
-#     :return: A list of the resized polygons.
-#     """
-#     return [
-#         scale(polygon, xfact=width, yfact=height, origin=(0, 0)) for polygon in polygons
-#     ]
+def draw_polygons(polygons, img, image_path, color):
+    for poly in polygons:
+        contours = [numpy.array(poly.exterior.coords).round().astype(numpy.int32)]
+        for contour in contours:
+            if len(contour) > 0:
+                img = cv2.drawContours(img, contours, 0, tuple(reversed(color)), -1)
+        cv2.imwrite(image_path, img)
+
+
+def resize_polygons(polygons: list, height: float, width: float) -> list:
+    """
+    Resize the polygons.
+    :param polygons: The polygons to resize.
+    :param height: The ratio in the height dimension.
+    :param width: The ratio in the width dimension.
+    :return: A list of the resized polygons.
+    """
+    return [
+        scale(polygon, xfact=width, yfact=height, origin=(0, 0)) for polygon in polygons
+    ]
+
 
 def split_polygons(polygons: list) -> list:
     """
@@ -98,18 +99,3 @@ def split_polygons(polygons: list) -> list:
     # Erode all polygons so that they don't touch when drawn over the label image.
     polygons = [poly.buffer(-2 * eps) for poly in polygons]
     return polygons
-
-
-label_polygons = {
-  "text_line": [
-    [[0,0], [100, 0], [100, 100], [0,100], [0,0]]
-  ],
-  "picture": [
-    [[100, 100], [500, 100], [500, 500], [100,500], [100,100]]
-  ],
-}
-label_colors = {
-  "text_line": (255, 0, 0), # red
-  "picture": (0, 0, 255), # blue
-}
-generate_mask(2600, 2102, label_polygons, label_colors, "doc_ufcn/train/test_ufcn.jpg") 
--- tests/test_mask.py
+++ tests/test_mask.py
@@ -1,36 +1,49 @@
 # -*- coding: utf-8 -*-
-import pytest
-from doc_ufcn.train.mask import generate_mask
 import cv2
+import numpy as np
+import pytest
+
+from doc_ufcn.train.mask import generate_mask
+
 
 @pytest.mark.parametrize(
-    "image_width, image_height, label_polygons, label_colors, output_path, true_image_path",
+    "image_width, image_height, label_polygons, label_colors, output_path, ground_truth_path",
     [
         (
-            2600, 
-            2102,
+            400,
+            500,
             {
                 "text_line": [
-                    [[0,0], [100, 0], [100, 100], [0,100], [0,0]]
+                    [[41, 280], [361, 280], [361, 311], [41, 311], [41, 280]]
                 ],
-                "picture": [
-                    [[100, 100], [500, 100], [500, 500], [100,500], [100,100]]
-                ],
+                "picture": [[[58, 76], [307, 76], [307, 205], [58, 205], [58, 76]]],
             },
-            {
-                "text_line": (255, 0, 0), # red
-                "picture": (0, 0, 255), # blue
-            },
-            "doc_ufcn/train/test.jpg"
+            {"text_line": (255, 0, 0), "picture": (0, 0, 255)},
+            "doc_ufcn/train/test.png",
+            "doc_ufcn/train/ground_truth.png",
         )
     ],
 )
-def test_generate_mask(image_width, image_height, label_polygons, label_colors, output_path, true_image_path):
-    generate_mask(image_width, image_height, label_polygons, label_colors, output_path, true_image_path)
-    image_path = output_path[:-4] + "_mask.jpg"
-    true_image = cv2.imread(true_image_path)
+def test_generate_mask(
+    image_width,
+    image_height,
+    label_polygons,
+    label_colors,
+    output_path,
+    ground_truth_path,
+):
+    generate_mask(image_width, image_height, label_polygons, label_colors, output_path)
+    image_path = output_path[:-4] + "_mask.png"
+    print(ground_truth_path)
+    truth_image = cv2.imread(ground_truth_path)
     image = cv2.imread(image_path)
-    errorL2 = cv2.norm(true_image, image, cv2.NORM_L2 )
-    similarity = 1 - errorL2 / ( image_height * image_width )
-    print('Similarity = ',similarity)
+
+    print("image : ", image.shape)
+    print("truth_image : ", truth_image.shape)
+    errorL2 = cv2.norm(truth_image, image, cv2.NORM_L2)
+    similarity = 1 - errorL2 / (image_height * image_width)
+    print("Similarity = ", similarity)
+
+    print("truth_image : ", np.unique(truth_image))
+    print("image : ", np.unique(image))
     assert similarity == 1
