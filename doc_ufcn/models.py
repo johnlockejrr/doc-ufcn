3ebcaa44213c95faff6a6ddfa806f71578f75692
Yoann Schneider <yschneider@teklia.com>
2022-11-14 14:04:17 +0100

    download from huggingface

--- doc_ufcn/models.py
+++ doc_ufcn/models.py
@@ -3,53 +3,33 @@
 import logging
 import os
 
-import requests
 import yaml
+from huggingface_hub import hf_hub_download
 
-from doc_ufcn.utils import md5sum
+HUGGING_FACE_REPO_PREFIX = "Teklia/doc-ufcn-"
 
-# Gitlab project: https://gitlab.com/teklia/dla/doc-ufcn
-GITLAB_PROJECT_ID = 30605923
-PACKAGES_URL = f"https://gitlab.com/api/v4/projects/{GITLAB_PROJECT_ID}/packages/"
+
+logger = logging.getLogger(__name__)
 
 
 def download_model(name, version=None):
+    cache_dir = os.environ.get("XDG_CACHE_HOME", os.path.expanduser("~/.cache"))
+    dir_path = os.path.join(cache_dir, "doc-ufcn", "models", name)
+    # Retrieve parameters.yml
+    parameters_path = hf_hub_download(
+        repo_id=HUGGING_FACE_REPO_PREFIX + name,
+        filename="parameters.yml",
+        cache_dir=dir_path,
+        revision=version,
+    )
+    # Retrieve parameters.yml
+    model_path = hf_hub_download(
+        repo_id=HUGGING_FACE_REPO_PREFIX + name,
+        filename="model.pth",
+        cache_dir=dir_path,
+        revision=version,
+    )
+    with open(parameters_path) as f:
+        parameters = yaml.safe_load(f)
 
-    base_dir = os.environ.get("XDG_CACHE_HOME", os.path.expanduser("~/.cache"))
-    dir_path = os.path.join(base_dir, "doc-ufcn", "models", name)
-    os.makedirs(dir_path, exist_ok=True)
-
-    model_path = os.path.join(dir_path, "model.pth")
-    parameters_path = os.path.join(dir_path, "parameters.yml")
-
-    # If no version given, get latest version.
-    if version is None:
-        packages = requests.get(
-            PACKAGES_URL + f"?package_name={name}&order_by=version&sort=desc"
-        )
-        packages.raise_for_status()
-        packages = yaml.safe_load(packages.content)
-        assert len(packages) > 0, f"Model {name} not available"
-        version = packages[0]["version"]
-
-    # Download and save the parameters.
-    parameters = requests.get(PACKAGES_URL + f"generic/{name}/{version}/parameters.yml")
-    parameters.raise_for_status()
-    logging.info(f"Loaded parameters: {name} (version {version})")
-    parameters = yaml.safe_load(parameters.content)
-    with open(parameters_path, "w") as f:
-        yaml.safe_dump(parameters, f)
-
-    # Check if model already in cache. Return the cached model and parameters.
-    if os.path.isfile(model_path) and md5sum(model_path) == parameters["md5sum"]:
-        logging.info(f"Loaded model from cache: {name} (version {version})")
-        return model_path, parameters["parameters"]
-
-    # Download the model if not in cache and save it.
-    model = requests.get(PACKAGES_URL + f"generic/{name}/{version}/model.pth")
-    model.raise_for_status()
-    logging.info(f"Loaded model: {name} (version {version})")
-    with open(model_path, "wb") as f:
-        f.write(model.content)
-
-    return model_path, parameters["parameters"]
+    return model_path, parameters
