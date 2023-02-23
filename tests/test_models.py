179ca0a4ade298d27f8cd41c538ebe8903bebae1
Yoann Schneider <yschneider@teklia.com>
2023-02-23 19:46:42 +0000

    More verbose huggingface errors

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,12 +3,13 @@
 import os
 
 import pytest
+from huggingface_hub.utils import RepositoryNotFoundError, RevisionNotFoundError
 
 from doc_ufcn import models
 
 
 @pytest.mark.parametrize(
-    "name, version, expected_model_path, expected_parameters",
+    "name, version, expected_model_path, expected_parameters, correct_name",
     [
         # Correct name and version
         (
@@ -16,6 +17,7 @@ from doc_ufcn import models
             "main",
             "~/.cache/doc-ufcn/models/generic-page/models--Teklia--doc-ufcn-generic-page/snapshots/ef5519d77baa190bfe294ad3994146dbe7e72095",
             pytest.lazy_fixture("test_parameters"),
+            True,
         ),
         # Correct fullname and version
         (
@@ -23,30 +25,40 @@ from doc_ufcn import models
             "main",
             "~/.cache/doc-ufcn/models/generic-page/models--Teklia--doc-ufcn-generic-page/snapshots/ef5519d77baa190bfe294ad3994146dbe7e72095",
             pytest.lazy_fixture("test_parameters"),
+            True,
         ),
         # Correct name and incorrect version
-        ("generic-page", "version", None, None),
+        ("generic-page", "version", None, None, True),
         # Correct name and no version
         (
             "generic-page",
             None,
             "~/.cache/doc-ufcn/models/generic-page/models--Teklia--doc-ufcn-generic-page/snapshots/ef5519d77baa190bfe294ad3994146dbe7e72095",
             pytest.lazy_fixture("test_parameters"),
+            True,
         ),
         # Incorrect name and incorrect version
-        ("page_model", "version", None, None),
+        ("page_model", "version", None, None, False),
         # Incorrect name and no version
-        ("page_model", None, None, None),
+        ("page_model", None, None, None, False),
     ],
 )
-def test_download_model(name, version, expected_model_path, expected_parameters):
+def test_download_model(
+    name, version, expected_model_path, expected_parameters, correct_name
+):
     """
     Test of the download_model function.
     Check that the correct model is loaded.
     """
     if expected_model_path is None and expected_parameters is None:
-        with pytest.raises(Exception):
-            model_path, parameters = models.download_model(name, version)
+        if correct_name:
+            # Bad model name
+            with pytest.raises(RevisionNotFoundError):
+                model_path, parameters = models.download_model(name, version)
+        else:
+            # Bad revision
+            with pytest.raises(RepositoryNotFoundError):
+                model_path, parameters = models.download_model(name, version)
     else:
         model_path, parameters = models.download_model(name, version)
         assert model_path == os.path.join(
