0a632ab9a3f25c9c39197bca5f2e673a17944103
NolanB <nboukachab@teklia.com>
2023-03-21 19:47:05 +0100

    Add a new config and a selector of models

--- hugging_face/config.py
+++ hugging_face/config.py
@@ -15,40 +15,14 @@ def parse_configurations(config_path: Path):
     """
     parser = ConfigParser()
 
-    model_parser = ConfigParser()
+    parser.add_option("title")
+    parser.add_option("description")
+    parser.add_option("examples", type=list)
+    model_parser = parser.add_subparser("models", many=True)
 
     model_parser.add_option("model_name", type=str)
     model_parser.add_option("classes_colors", type=list, default=["green"])
-    model_parser.add_option("title", type=str)
-    model_parser.add_option("description", type=str)
-    model_parser.add_option("examples", type=list)
 
 
-    parser.add_subparser("models", model_parser,type=list,default=[])
 
     return parser.parse(config_path)
-
-
-def parse_yaml(config_path: Path):
-
-
-    parser = ConfigParser()
-
-    # model_parser = ConfigParser()
-    models = parser.add_subparser("models", default={})
-    name = models.add_subparser("doc-ufcn-generic-historical-line", default=[])
-    
-    # for model in parser["models"]:
-    #     print(model["model_name"])
-
-
-    name.add_option("model_name", type=str)
-    name.add_option("classes_colors", type=list, default=["green"])
-    name.add_option("title", type=str)
-    name.add_option("description", type=str)
-    name.add_option("examples", type=list)
-
-    
-    log = parser.parse(config_path)
-    
-    print(log)
