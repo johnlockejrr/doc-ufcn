1249433b2aaba3fbe04cd7f46d097d995d6a8376
NolanB <nboukachab@teklia.com>
2023-03-22 17:52:37 +0100

    Add a multiple examples and dynamic model description

--- hugging_face/config.py
+++ hugging_face/config.py
@@ -17,11 +17,13 @@ def parse_configurations(config_path: Path):
 
     parser.add_option("title")
     parser.add_option("description")
-    parser.add_option("examples", type=list)
     model_parser = parser.add_subparser("models", many=True)
 
     model_parser.add_option("model_name", type=str)
+    model_parser.add_option("title")
+    model_parser.add_option("description")
     model_parser.add_option("classes_colors", type=list, default=["green"])
+    model_parser.add_option("examples", type=list)
 
 
 
