ea327913c7e096f4237e1c99a7bec57a4e20ccd8
NolanB <nboukachab@teklia.com>
2023-03-27 17:26:08 +0200

    WIP: try to modif with class

--- hugging_face/app.py
+++ hugging_face/app.py
@@ -2,8 +2,8 @@
 
 import argparse
 import json
-import os
 from pathlib import Path
+from typing import NamedTuple
 
 import gradio as gr
 import numpy as np
@@ -39,20 +39,49 @@ config = parse_configurations(args.config)
 models_name = [model["model_name"] for model in config["models"]]
 
 
-def load_model(model_name, model_id):
-    """
-    Load a model by name
+class UFCNModel(NamedTuple):
+    name: str
+    classes: list
+    colors: list
+    model: DocUFCN
+    title: str
+    description: str
 
-    :param model_name: The name of the model to load
-    :param model_id: The id of the current model
-    :return: The classes, the colors by classes and the model
+    def get_class_name(self, channel_idx):
+        return self.classes[channel_idx]
+
+
+# Cached models, maps model_name to UFCNModel object
+MODELS = {}
+
+
+def load_model(model_name):
     """
+    Load a model by name if it doesn't already exist then return the model
+
+    :param model_name: The name of the selected model
+    :return: The UFCNModel instance selected
+    """
+
+    if model_name in MODELS:
+        # If cached already, return cached model
+        return MODELS[model_name]
+
+    # Create a new model
 
     # Download the model
     model_path, parameters = models.download_model(name=model_name)
 
+    model_id = get_model_idx(model_name)
+
     # Store classes_colors list
     classes_colors = config["models"][model_id]["classes_colors"]
+
+    # Store title
+    title = config["models"][model_id]["title"]
+
+    # Store description
+    description = config["models"][model_id]["description"]
 
     # Store classes
     classes = parameters["classes"]
@@ -64,8 +93,8 @@ def load_model(model_name, model_id):
 
     # Check that the paths of the examples are valid
     for example in config["examples"]:
-        assert os.path.exists(
-            example
+        assert Path.exists(
+            Path(example)
         ), f"The path of the image '{example}' does not exist."
 
     # Load the model
@@ -76,14 +105,22 @@ def load_model(model_name, model_id):
     )
     model.load(model_path=model_path, mean=parameters["mean"], std=parameters["std"])
 
-    return classes, classes_colors, model
+    MODELS[model_name] = UFCNModel(
+        name=model_name,
+        classes=classes,
+        colors=classes_colors,
+        model=model,
+        title=title,
+        description=description,
+    )
+    return MODELS[model_name]
 
 
-def query_image(dropdown, image):
+def query_image(model, image):
     """
     Load a model and draws the predicted polygons with the color provided by the model on an image
 
-    :param dropdown: A model selected in dropdown
+    :param model: A model selected in dropdown
     :param image: An image to predict
     :return: Image and dict, an image with the predictions and a
         dictionary mapping an object idx (starting from 1) to a dictionary describing the detected object:
@@ -91,14 +128,12 @@ def query_image(dropdown, image):
         - `confidence` key : float, confidence of the model,
         - `channel` key : str, the name of the predicted class.
     """
-    # Get the id of the model in the list of models
-    model_id = models_name.index(dropdown)
 
-    # Load the model and get its classes, classes_colors and the model
-    classes, classes_colors, model = load_model(dropdown, model_id)
+    # # # Load the model and get its classes, classes_colors and the model
+    # Model = load_model(model)
 
     # Make a prediction with the model
-    detected_polygons, probabilities, mask, overlap = model.predict(
+    detected_polygons, probabilities, mask, overlap = MODELS[model].model.predict(
         input_image=image, raw_output=True, mask_output=True, overlap_output=True
     )
 
@@ -113,12 +148,12 @@ def query_image(dropdown, image):
 
     # Create the polygons on the copy of the image for each class with the corresponding color
     # We do not draw polygons of the background channel (channel 0)
-    for channel in range(1, len(classes)):
+    for channel in range(1, len(MODELS[model].classes)):
         for i, polygon in enumerate(detected_polygons[channel]):
             # Draw the polygons on the image copy.
             # Loop through the class_colors list (channel 1 has color 0)
             ImageDraw.Draw(img2).polygon(
-                polygon["polygon"], fill=classes_colors[channel - 1]
+                polygon["polygon"], fill=MODELS[model].colors[channel - 1]
             )
 
             # Build the dictionary
@@ -131,7 +166,7 @@ def query_image(dropdown, image):
                     # Confidence that the model predicts the polygon in the right place
                     "confidence": polygon["confidence"],
                     # The channel on which the polygon is predicted
-                    "channel": classes[channel],
+                    "channel": MODELS[model].classes[channel],
                 }
             )
 
@@ -139,9 +174,9 @@ def query_image(dropdown, image):
     return Image.blend(image, img2, 0.5), json.dumps(predict, indent=20)
 
 
-def get_value(dropdown):
+def get_model_idx(dropdown):
     """
-    Get the model selected in dropdown component and return this id
+    Get the model selected in dropdown component and return its id
 
     :param dropdown: A model selected in dropdown
     :return: The model id selected
@@ -149,30 +184,36 @@ def get_value(dropdown):
     return models_name.index(dropdown)
 
 
-def change_model_title(model_id):
+def update_model(model_name):
     """
-    Change the model title to the title of the current model
+    Update MODELS dict
+
+    :param model_name: The name of the current model
+    """
+    MODELS[model_name] = load_model(model_name)
+
+
+def update_model_title(model_name):
+    """
+    Update the model title to the title of the current model
 
     :param model_id: The id of the current model
     :return: A new title
     """
-    return f"## {config['models'][model_id]['title']}"
+    return f"## {MODELS[model_name].title}"
 
 
-def change_model_description(model_id):
+def update_model_description(model_name):
     """
-    Change the model description to the description of the current model
+    Update the model description to the description of the current model
 
     :param model_id: The id of the current model
     :return: A new description
     """
-    return config["models"][model_id]["description"]
+    return MODELS[model_name].description
 
 
 with gr.Blocks() as process_image:
-    # Create a int Number for define the model id
-    model_id = gr.Number(precision=0, value=0, visible=False)
-
     # Create app title
     title = gr.Markdown(f"# {config['title']}")
 
@@ -180,24 +221,23 @@ with gr.Blocks() as process_image:
     description = gr.Markdown(config["description"])
 
     # Create dropdown button
-    dropdown = gr.Dropdown(models_name, value=models_name[0], label="Models")
+    model_name = gr.Dropdown(models_name, value=models_name[0], label="Models")
 
-    # Create model title
-    model_title = gr.Markdown(f"## {config['models'][model_id.value]['title']}")
+    load_model(model_name.value)
+
+    model_title = gr.Markdown(f"## {MODELS[model_name.value].title}")
+    # model_title = gr.Markdown(f"## {config['models'][model_id.value]['title']}")
 
     # Create model description
-    model_description = gr.Markdown(config["models"][model_id.value]["description"])
+    model_description = gr.Markdown(MODELS[model_name.value].description)
 
-    # Set the model id to the selected model id by the dropdown button
-    dropdown.change(get_value, dropdown, model_id)
+    model_name.change(update_model, model_name)
 
     # Change model title when the model_id is update
-    model_id.change(change_model_title, model_id, model_title)
+    model_name.change(update_model_title, model_name, model_title)
 
     # Change model description when the model_id is update
-    model_description = model_id.change(
-        change_model_description, model_id, model_description
-    )
+    model_name.change(update_model_description, model_name, model_description)
 
     # Create a first row of blocks
     with gr.Row():
@@ -242,7 +282,7 @@ with gr.Blocks() as process_image:
 
     # Create the button to submit the prediction
     submit_button.click(
-        query_image, inputs=[dropdown, image], outputs=[image_output, json_output]
+        query_image, inputs=[model_name, image], outputs=[image_output, json_output]
     )
 
 # Launch the application with the public mode (True or False)
