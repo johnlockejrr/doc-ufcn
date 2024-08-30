0265bf37aaa36fa5a2e320152a4effdf5f4cdc4d
johnlockejrr <johnlockejrr@gmail.com>
2024-08-30 04:58:14 -0700

    Update models.py
    Set the global variable `HUGGING_FACE_REPO_PREFIX = ""` and comment the line `name = name.replace("doc-ufcn-", "")` to comply with `huggin_face/config.yaml`

--- doc_ufcn/models.py
+++ doc_ufcn/models.py
@@ -6,7 +6,7 @@ import yaml
 from huggingface_hub import hf_hub_download
 from huggingface_hub.utils import RepositoryNotFoundError, RevisionNotFoundError
 
-HUGGING_FACE_REPO_PREFIX = "Teklia/doc-ufcn-"
+HUGGING_FACE_REPO_PREFIX = ""
 
 
 logger = logging.getLogger(__name__)
@@ -14,7 +14,6 @@ logger = logging.getLogger(__name__)
 
 def download_model(name, version=None):
     # Strip the model name prefix if provided
-    name = name.replace("doc-ufcn-", "")
     logger.info(f"Will look for model @ {HUGGING_FACE_REPO_PREFIX + name}")
 
     cache_dir = Path(os.environ.get("XDG_CACHE_HOME", Path("~/.cache").expanduser()))
